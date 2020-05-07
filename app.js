ca08dddf02568fc7a46893b810780e93a7717eb6
Viacheslav <little.cado1@gmail.com>
2020-05-07 19:55:56 +0300

    Add function to create list items and add styles for them

--- app.js
+++ app.js
@@ -10,4 +10,26 @@ todoButton.addEventListener('click', addTodo);
 function addTodo(event){
     //prevent for form submitting
     event.preventDefault();
+    //to do div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add('todo');
+    //Create li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    //Check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    //Append to list
+    todoList.appendChild(todoDiv);
+    //Clear todo input value
+    todoInput.value = '';
 }
